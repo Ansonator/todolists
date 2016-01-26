2c3f6051e0012e3924e8580e7d591a7735bfee89
ba030483 <bryce.anson@cerner.com>
2016-01-26 10:55:33 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126003910) do
+ActiveRecord::Schema.define(version: 20160126034651) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender",     limit: 6
-    t.decimal  "birth_year",           precision: 4
+    t.integer  "birth_year"
     t.string   "first_name"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.string   "last_name"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
   end
 
   create_table "todo_items", force: :cascade do |t|
