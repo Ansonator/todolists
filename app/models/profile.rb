91cb443dfc80f50103c770712a295618a3931503
ba030483 <bryce.anson@cerner.com>
2016-01-29 22:45:06 -0600

    final

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,32 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_name_or_last_name_present
+  validate :male_not_named_sue
+  validates :gender, inclusion: { 
+    in: %w(male female),
+    message: "%{value} is not a valid gender" 
+  }
+
+  scope :get_all_profiles, -> (min_year, max_year){
+    where("birth_year between ? and ?", min_year, max_year).
+      order(:birth_year)
+  }
+
+  def first_name_or_last_name_present
+    if first_name.nil? and last_name.nil?
+      msg = 'either first_name or last_name must be present'
+      errors.add(:first_name, msg)
+      errors.add(:last_name, msg)
+    end
+  end
+
+  def male_not_named_sue
+    if gender == 'male' && first_name == 'Sue'
+      msg = "a male cannot be named #{first_name}"
+      errors.add(:first_name, msg)
+      errors.add(:gender, msg)
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
+
+lines = [
+  ['Carly Fiorina', 1954, 'female'],
+  ['Donald Trump', 1946, 'male'],
+  ['Ben Carson', 1951, 'male'],
+  ['Hillary Clinton', 1947, 'female']
+]
+
+dueDate = Date.today + 1.year
+lines.each do |line|
+
+  names = line[0].split
+  password = line[0].downcase.sub('\s', '')
+  user = User.create!(username: names[1], password_digest: password)
+
+  user.create_profile!(gender: line[2], birth_year: line[1], first_name: names[0], last_name: names[1])
+
+  list = TodoList.create!(list_name: "list#{user.id}", list_due_date: dueDate)
+  (1..5).each do |i|
+    str = "#{user.id}:#{i}"
+    list.todo_items << TodoItem.create!(title: "title:#{str}", description: "desc #{str}", due_date: dueDate, completed: false)
+  end
+  user.todo_lists = [list]
+
+end
